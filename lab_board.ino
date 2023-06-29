98127e19ab5e081cc6404b7416fc12196e053011
Sergey Dobychin <dobychin@gmail.com>
2023-06-29 09:20:14 +0300

    Revert

--- lab_board.ino
+++ lab_board.ino
@@ -1068,7 +1068,6 @@ void sendSourceData(uint8_t number, uint32_t timestamp, uint8_t index)
   uint8_t inputsUpdated = 0;
   uint8_t dataOffset;
   device_t *device;
-  uint8_t flags;
   for (uint8_t deviceIndex = 0; deviceIndex < deviceCount; deviceIndex++)
   {
     device = devices[deviceIndex];
@@ -1077,8 +1076,7 @@ void sendSourceData(uint8_t number, uint32_t timestamp, uint8_t index)
       continue;
     }
     dataOffset = inputsUpdated * (sizeof(device_d_t) + 1) + sizeof(timestamp);
-    flags = device->flags & (DEVICE_FLAG_MONIRORING_MASK * (1 - min(device->flags & DEVICE_FLAG_MUTED, 1)));
-    messageOut->data[dataOffset + 1] = deviceIndex | flags;
+    messageOut->data[dataOffset + 1] = deviceIndex | (device->flags & DEVICE_FLAG_MONIRORING_MASK);
     memcpy(&messageOut->data[dataOffset + 2], &device->data, sizeof(device_d_t));
     inputsUpdated++;
 
